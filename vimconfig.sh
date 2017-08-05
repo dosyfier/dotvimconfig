1ca845e9bb89e52b2ea6a9766c4030f96948f994
dosyfier <christophe.sourisse@gmail.com>
2017-08-05 23:33:27 +0200

    Add of a removal function to vimconfig script.

--- vimconfig.sh
+++ vimconfig.sh
@@ -6,9 +6,15 @@ function usage() {
   echo ""
   echo "Detailed commands:"
   echo "******************"
-  echo "vim init             -> Initializes Vim configuration"
-  echo "vim add [plugin-url] -> Register a new Vim plugin into your configuration"
-  echo "vim update           -> Update all registered Vim plugins at once"
+  echo "vimconfig init                 -> Initializes Vim configuration"
+  echo "vimconfig add [plugin-url]     -> Register a new Vim plugin into your configuration"
+  echo "vimconfig remove [plugin-name] -> Delete some Vim plugin from your configuration"
+  echo "vimconfig update               -> Update all registered Vim plugins at once"
+  echo ""
+  echo "Examples:"
+  echo "*********"
+  echo "vimconfig add https://github.com/tpope/vim-surround"
+  echo "vimconfig remove vim-surround"
   echo ""
 }
 
@@ -33,24 +39,35 @@ function add() {
   fi
 }
 
+function remove() {
+  if [[ -d bundle/$1 ]]; then
+    git submodule deinit bundle/$1
+    git rm bundle/$1
+    git commit -m "Remove $1 bundle from submodules list."
+  else
+    echo "The $1 plugin does not exist under bundle/. Doing nothing..."
+    exit 2
+  fi
+}
+
 function update() {
   git submodule foreach git pull origin master
 }
 
 
-if [[ $# -gt 2 || ( $1 -ne 'add' && $# -gt 1 ) ]]; then
-  usage
-
-elif [[ $1 == 'init' ]]; then
+if [[ $1 == 'init' && $# -eq 1 ]]; then
   init
 
-elif [[ $1 == 'add' ]]; then
+elif [[ $1 == 'add' && $# -eq 2 ]]; then
   add $2
 
-elif [[ $1 == 'update' ]]; then
+elif [[ $1 == 'remove' && $# -eq 2 ]]; then
+  remove $2
+
+elif [[ $1 == 'update' && $# -eq 1 ]]; then
   update
 
 else
   usage
 fi
-  
+
