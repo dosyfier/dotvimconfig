9ed5d0ae7611556992f9bda6656b340e9825a7a7
dosyfier <christophe.sourisse@gmail.com>
2018-04-02 11:35:17 +0200

    Making vimconfig.sh callable from anywhere (& adding comments).

--- vimconfig.sh
+++ vimconfig.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 
-function usage() {
+usage() {
   cat <<EOF
 
-Usage: vimconfig <init|add|update> [plugin-url]
+Usage: vimconfig [OPTIONS] <init|add|update> [plugin-url]
 
 Detailed commands:
-******************
+------------------
 vimconfig init                 -> Initializes Vim configuration
 vimconfig add [plugin-url]     -> Register a new Vim plugin into your configuration
 vimconfig remove [plugin-name] -> Delete some Vim plugin from your configuration
 vimconfig update               -> Update all registered Vim plugins at once
 
+Options:
+--------
+-h | --help  -> Display this message
+
 Examples:
-*********
+---------
 vimconfig add https://github.com/tpope/vim-surround
 vimconfig remove vim-surround
 
 EOF
 }
 
-function init() {
-  [[ -f ~/.vimrc ]] && rm -f ~/.vimrc
-  ln -s ~/.vim/vimrc ~/.vimrc
-  cd ~/.vim
+# Run the provided command & arguments into dotvimconfig project's directory
+run_in_project() {
+  pushd "$(dirname $0)" > /dev/null
+  $@
+  return_code=$?
+  popd > /dev/null
+  return $?
+}
+
+# Initializes Vim configuration
+init() {
+  # Build this script's effective parent directory
+  # by resolving links and/or trailing ~
+  real_dirname="`readlink -f $(dirname $0)`"
+  real_dirname="${real_dirname/#\~/$HOME}"
+
+  # If this script isn't launched from ~/.vim dir, then force rebuilding 
+  # ~/.vim from the actual dotvimconfig project directory
+  if ! [ "$real_dirname" = "$HOME/.vim" ] ; then
+    rm -rf "$HOME/.vim"
+    rm -f ~/.vimrc
+    ln -s "$real_dirname" "$HOME/.vim"
+    ln -s ~/.vim/vimrc ~/.vimrc
+  fi
+ 
   git submodule init
   git submodule update
 }
 
-function add() {
+# Register a new Vim plugin into Vim configuration
+add() {
+  # Parse the provided plugin URL to extract the plugin's Git repo name 
+  # and remove any trailing ".git" extension
   url_parts=(`echo $1 | grep -Po '[^\/]+'`)
   plugin_name=${url_parts[-1]/%.git/}
-  if [[ -d bundle/$plugin_name ]]; then
+
+  if [ -d bundle/$plugin_name ]; then
     echo "The $plugin_name plugin already exists under bundle/. Doing nothing..."
     exit 2
   else
@@ -41,8 +70,9 @@ function add() {
   fi
 }
 
-function remove() {
-  if [[ -d bundle/$1 ]]; then
+# Delete some Vim plugin from Vim configuration
+remove() {
+  if [ -d bundle/$1 ]; then
     git submodule deinit bundle/$1
     git rm bundle/$1
     git commit -m "Remove $1 bundle from submodules list."
@@ -52,24 +82,23 @@ function remove() {
   fi
 }
 
-function update() {
+# Update all registered Vim plugins at once
+update() {
   git submodule foreach git pull origin master
 }
 
 
-if [[ $1 == 'init' && $# -eq 1 ]]; then
-  init
+# -- Main program -- #
 
-elif [[ $1 == 'add' && $# -eq 2 ]]; then
-  add $2
-
-elif [[ $1 == 'remove' && $# -eq 2 ]]; then
-  remove $2
-
-elif [[ $1 == 'update' && $# -eq 1 ]]; then
-  update
-
-else
+command=$1
+if [[ $command =~ (-h|--help) ]]; then
   usage
+  exit 0
+elif [ -z $command ] || ! [[ $command =~ (init|add|remove|update) ]] \
+  || [ $# -gt 2 ] || ( ! [[ $command =~ (add|remove) ]] && [ $# -gt 1 ] ); then
+  usage
+  exit 1
+else
+  run_in_project $@
 fi
 
