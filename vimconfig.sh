052feba325a1d7340fcaead57e51ca41c4783a9c
csouriss <christophe.sourisse@capgemini.com>
2019-01-28 12:39:28 +0100

    Miscellaneous corrections/enhancements in vimconfig.sh

--- vimconfig.sh
+++ vimconfig.sh
@@ -27,9 +27,9 @@ EOF
 # Run the provided command & arguments into dotvimconfig project's directory
 run_in_project() {
   set -e
-  pushd "$(dirname $0)" > /dev/null
+  pushd "$HOME/.vim" > /dev/null
   trap "popd > /dev/null" EXIT
-  $@
+  "$@"
 }
 
 # Ask the user for the deletion of the file or directory provided as argument:
@@ -41,12 +41,19 @@ remove_or_abort() {
     while ! [[ $REPLY =~ ^[yn]$ ]]; do
       read -p "Override? [y/n] " -r
     done
-    if [ $REPLY = y ]; then
+    if [ "$REPLY" = y ]; then
       rm -rf "$1"
     else
       return 2
     fi
   fi
+}
+
+# Parse the provided plugin URL to extract the plugin's Git repo name 
+# and remove any trailing ".git" extension
+to_bundle_name() {
+  mapfile -t url_parts < <(echo "$1" | grep -Po '[^\/]+')
+  echo "${url_parts[-1]/%.git/}"
 }
 
 # Initializes Vim configuration
@@ -55,13 +62,13 @@ init() {
   # by resolving links and/or trailing ~
   # (using pwd since we are necessarily in dotvimconfig dir,
   # and because, due to run_in_project, '$(dirname $0)' would be wrong)
-  real_dirname="`readlink -f $(pwd)`"
+  real_dirname="$(readlink -f "$(pwd)")"
   real_dirname="${real_dirname/#\~/$HOME}"
 
   # If this script isn't launched from ~/.vim dir, then force rebuilding 
   # ~/.vim from the actual dotvimconfig project directory
   echo "Init $HOME/.vim..."
-  if ! [ "$real_dirname" = "$(readlink -f $HOME/.vim)" ] ; then
+  if ! [ "$real_dirname" = "$(readlink -f "$HOME/.vim")" ] ; then
     remove_or_abort "$HOME/.vim"
     ln -s "$real_dirname" "$HOME/.vim"
   fi
@@ -69,9 +76,9 @@ init() {
   # Erase existing .vimrc, and warn the user about it first
   echo "Init $HOME/.vimrc..."
   if ! [ -e "$HOME/.vimrc" ] || ! [ -L "$HOME/.vimrc" ] || \
-	! [ "$real_dirname/vimrc" = "$(readlink -f $HOME/.vimrc)" ]; then
+	! [ "$real_dirname/vimrc" = "$(readlink -f "$HOME/.vimrc")" ]; then
     remove_or_abort "$HOME/.vimrc"
-    ln -s $HOME/.vim/vimrc $HOME/.vimrc
+    ln -s "$HOME/.vim/vimrc" "$HOME/.vimrc"
   fi
 
   echo "Download and/or update Vim modules..."
@@ -82,30 +89,31 @@ init() {
 }
 
 # Register a new Vim plugin into Vim configuration
+# $1 : New plugin's URL
 add() {
-  # Parse the provided plugin URL to extract the plugin's Git repo name 
-  # and remove any trailing ".git" extension
-  url_parts=(`echo $1 | grep -Po '[^\/]+'`)
-  plugin_name=${url_parts[-1]/%.git/}
+  plugin_name="$(to_bundle_name "$1")"
 
-  if [ -d bundle/$plugin_name ]; then
+  if [ -d "bundle/$plugin_name" ]; then
     echo "The $plugin_name plugin already exists under bundle/. Doing nothing..."
     return 2
   else
-    git submodule add $1 bundle/$plugin_name
-    git add bundle/$plugin_name
+    git submodule add "$1" "bundle/$plugin_name"
+    git add "bundle/$plugin_name"
     git commit -m "Install $plugin_name bundle as a submodule."
   fi
 }
 
 # Delete some Vim plugin from Vim configuration
+# $1 : The name or URL of the plugin to remove
 remove() {
-  if [ -d bundle/$1 ]; then
-    git submodule deinit bundle/$1
-    git rm bundle/$1
-    git commit -m "Remove $1 bundle from submodules list."
+  plugin_name="$(to_bundle_name "$1")"
+
+  if [ -d "bundle/$plugin_name" ]; then
+    git submodule deinit "bundle/$plugin_name"
+    git rm "bundle/$plugin_name"
+    git commit -m "Remove $plugin_name bundle from submodules list."
   else
-    echo "The $1 plugin does not exist under bundle/. Doing nothing..."
+    echo "The $plugin_name plugin does not exist under bundle/. Doing nothing..."
     return 2
   fi
 }
@@ -123,11 +131,11 @@ command=$1
 if [[ $command =~ (-h|--help) ]]; then
   usage
   exit 0
-elif [ -z $command ] || ! [[ $command =~ (init|add|remove|update) ]] \
-  || [ $# -gt 2 ] || ( ! [[ $command =~ (add|remove) ]] && [ $# -gt 1 ] ); then
+elif [ -z "$command" ] || ! [[ $command =~ (init|add|remove|update) ]] \
+  || [ $# -gt 2 ] || { ! [[ $command =~ (add|remove) ]] && [ $# -gt 1 ] ;}; then
   usage
   exit 1
 else
-  run_in_project $@
+  run_in_project "$@"
 fi
 
